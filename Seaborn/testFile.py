88834a95e6be939423242de61b86ebbfd198276c
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-10 17:20:28 +0530

    new

--- Seaborn/testFile.py
+++ Seaborn/testFile.py
@@ -1,4 +1,5 @@
 import seaborn as sns
+import streamlit as st
 import matplotlib.pyplot as plt
 
 data = sns.load_dataset("iris")
@@ -6,4 +7,4 @@ data = sns.load_dataset("iris")
 # draw lineplot
 sns.lineplot(x="sepal_length", y="sepal_width", data=data)
 plt.title('Title using Matplotlib Function')
-plt.show()
+st.pyplot(plt)
