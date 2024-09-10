e1ba8cda23ff04a23a83cc733e15b3ab3769fed7
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-10 15:26:46 +0530

    new

--- Matplotlib/Piecharts.py
+++ Matplotlib/Piecharts.py
@@ -2,21 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 y = np.array([35, 25, 25, 15])
-
-data = [35, 25, 25, 15]
-
-plt.pie(data, y)
-plt.show()
-
-y = np.array([35, 25, 25, 15])
 mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
-
-plt.pie(data, y, labels = mylabels)
-plt.show()
-
-y = np.array([35, 25, 25, 15])
-mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
-myexplode = [0.2, 0, 0, 0]
+myexplode = [0.1, 0, 0, 0]
 
 plt.pie(y, labels = mylabels, explode = myexplode)
-plt.show()
+plt.show() 
