335f80c75993c350dbaccc0ff417605e2ab417d1
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-29 12:50:42 +0530

    new

--- Matplotlib/matplotlibtest.py
+++ Matplotlib/matplotlibtest.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpoint = np.array([6,0])
-ypoint = np.array([2,10])
-plt.plot(xpoint,ypoint)
+# xpoint = np.array([6,0])
+# ypoint = np.array([2,10])
+# plt.plot(xpoint,ypoint)
+# plt.show()
+
+
+x = np.array([1,2])
+y = np.array([4,9])
+plt.scatter(x,y)
 plt.show()
